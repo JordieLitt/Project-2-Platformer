1ceb82f318442429965cbc3540a7a970eaf27141
Jordan Little <Jordielitt@knights.ucf.edu>
2020-10-25 19:47:59 -0400

    challenge commit

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+   public float speed;
+   public bool moveRight;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(moveRight)
+        {
+            transform.Translate(2 * Time.deltaTime *speed , 0, 0);
+            transform.localScale = new Vector2 (.35f,.35f);
+        }
+        else
+        {
+            transform.Translate(-2 * Time.deltaTime *speed , 0, 0);
+            transform.localScale = new Vector2 (-.35f,.35f);
+        }
+    }
+    void OnTriggerEnter2D(Collider2D trig)
+    {
+        if(trig.gameObject.CompareTag("Turn"))
+        {
+            if (moveRight)
+            {
+                moveRight = false;
+            }
+            else
+            {
+                moveRight = true;
+            }
+        }
+    }
+}
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -6,49 +6,146 @@ using UnityEngine.UI;
 public class PlayerScript : MonoBehaviour
 {
     private Rigidbody2D rd2d;
-    public float speed;
+    private Animator animator;
     public Text score;
     private int scoreValue = 0;
-    // Start is called before the first frame update
+    public Text livesText;
+    private int livesCount = 3;
+    public Text winText;
+    private float hozMovement;
+    private float verMovement;
+    [Header("Movement")]
+    public float currentSpd;
+    public float maxSpd;
+    public float accel;
+    private bool facingRight;
+    public LayerMask whatIsGround;
+    public float jumpForce;
+    public AudioSource musicSource;
+    public AudioClip musicClipDeath;
+    public AudioClip musicClipHit;
+    
+
+    
+
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
-        score.text = scoreValue.ToString();
+        winText.text = " ";
+        SetScore();
+        SetLivesText();
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        hozMovement = Input.GetAxisRaw("Horizontal");
+        verMovement = Input.GetAxis("Vertical");
+
+        if (Mathf.Abs (hozMovement) > 0)
+        {
+            currentSpd += Time.deltaTime * accel;
+            currentSpd = Mathf.Clamp(currentSpd, 5, maxSpd);
+            
+        }
+        else
+        {
+            currentSpd = Time.deltaTime * 0;
+            currentSpd = Mathf.Clamp(currentSpd, 0, maxSpd);
+        }
+        bool isTouchingGround = Physics2D.Raycast(transform.position, Vector2.down, 2.205f, whatIsGround);
+        if (Input.GetKeyDown(KeyCode.W) && isTouchingGround == true)
+        {
+            rd2d.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            
+        }
+        if (scoreValue == 8)
+        {
+            winText.text = "You Win! Game Created By:Jordan Little";
+        }
+      
+       if ( isTouchingGround == false)
+       {
+           animator.SetInteger("State",1);
+           animator.SetFloat("Speed", 0);
+       }
+       else
+       {
+            animator.SetInteger("State",0);
+            
+       }
+         animator.SetFloat("Speed", currentSpd);
+       
+      
+       
         if (Input.GetKey("escape"))
         {
             Application.Quit();
         }
+        
+        Flip(hozMovement);
+        
+    }
+
+    private void Flip(float hozMovement)
+    {
+        if (hozMovement > 0 && facingRight || hozMovement < 0 && !facingRight)
+        {
+            facingRight = !facingRight;
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
     }
     void FixedUpdate()
     {
-        float hozMovement = Input.GetAxis("Horizontal");
-        float verMovement = Input.GetAxis("Vertical");
-
-        rd2d.AddForce(new Vector2(hozMovement * speed, verMovement * speed));
-
+       
+        rd2d.velocity = new Vector2(hozMovement * currentSpd, rd2d.velocity.y); 
     }
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.collider.tag == "Coin")
+        if(collision.CompareTag("Coin"))
         {
             scoreValue += 1;
-            score.text = scoreValue.ToString();
-            Destroy(collision.collider.gameObject);
+            SetScore();
+            Destroy(collision.gameObject);
+        }
+        if(collision.CompareTag("Enemy"))
+        {
+            livesCount -= 1;
+            SetLivesText();
+        }
+        if (scoreValue == 4)
+        {
+            transform.position = new Vector3(80, 14, 0);
+        }
+        
+
+    }
+    void SetScore()
+    {
+        score.text = "Score: " +scoreValue.ToString ();
+        if (scoreValue == 4)
+        {
+            livesCount = 3;
+            livesText.text = "Lives: " + livesCount.ToString();
+            
         }
     }
-    private void OnCollisionStay2D(Collision2D collision)
+    void SetLivesText()
     {
-        if(collision.collider.tag == "Ground")
+        livesText.text = "Lives: " + livesCount.ToString();
+        if (livesCount == 0)
         {
-            if(Input.GetKey(KeyCode.W))
-            {
-                rd2d.AddForce(new Vector2(0,3),ForceMode2D.Impulse);
-            }
+            winText.text = "Game Over";
+            Destroy(this.gameObject);
+            musicSource.clip = musicClipDeath;
+            musicSource.Play();
+        }
+        if(livesCount < 3 && livesCount > 0)
+        {
+            musicSource.clip = musicClipHit;
+            musicSource.Play();
         }
     }
 }
